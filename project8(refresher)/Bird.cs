c349b6c1c1c6f3f9ed55db793203a9aab2a71ec8
Chris Cooper <bloodwolf444@gmail.com>
2021-05-27 13:39:26 -0700

    added a bird class and added some static classes to the program class.

---
+++ project8(refresher)/Bird.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project8_refresher_
+{
+    class Bird
+    {
+        public string Color { get; set; }
+        public string Species { get; set; }
+
+        public Bird(string color, string species)
+        {
+            Color = color;
+            Species = species;
+        }
+    }
+}
--- project8(refresher)/Program.cs
+++ project8(refresher)/Program.cs
@@ -10,6 +10,49 @@ namespace project8_refresher_
     {
         static void Main(string[] args)
         {
+            
+            int number1 = 10;
+            number1 += 10;
+            Console.WriteLine(number1);
+            Console.WriteLine(add(10, 15));
+            askMathQuestion(256, 567);
+
+            Bird robin = new Bird("red", "robin");
+            Console.WriteLine(robin.Species);
+            
+            
+            Console.ReadLine();
+        }
+
+
+        static void askMathQuestion(int firstNumber, int secondnumber)
+        {
+            int correctAnswer = firstNumber + secondnumber;
+            int answerAsInt = 0;
+            Console.WriteLine($"what is {firstNumber} + {secondnumber}");
+
+            while (correctAnswer != answerAsInt)
+            {
+             
+                    string guess = Console.ReadLine();
+                    answerAsInt = int.Parse(guess);
+                    if (correctAnswer == answerAsInt)
+                    {
+                        Console.WriteLine("Correct answer. good job. Press any key to exit");
+                        break;
+                    }
+                    else { Console.WriteLine("Sorry please try again."); }
+            }
+      
+            
+        }
+
+        static double add(int firstNumber, int secondNumber)
+        {
+            double firstN = firstNumber;
+            double secondN = secondNumber;
+
+            return firstN + secondN;
         }
     }
 }
